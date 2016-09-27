92a2bcd9d55120954f1e804e3da9bedcf360a09e
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2016-09-26 22:37:31 -0300

    first commit

---
+++ bnc_oled/src/Sensor.c
@@ -0,0 +1,101 @@
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_ssp.h"
+#include "lpc17xx_i2c.h"
+#include "lpc17xx_timer.h"
+#include "oled.h"
+#include "light.h"
+
+
+uint32_t Light_Interrupt_Done_Flag = 0;
+static void init_i2c(void);
+static void init_ssp(void);
+
+
+typedef struct tag_sensor
+{
+	int sensor_data;
+}ttag_sensor;
+
+
+ttag_sensor ClassHandle;
+
+
+void Sensor_new(void)
+{	init_ssp();
+	init_i2c();
+
+	SysTick_Config(SystemCoreClock / 10);
+	oled_init();
+
+	light_init();
+	light_enable();
+}
+
+int Sensor_read(void)
+{
+	Light_Interrupt_Done_Flag = 0;
+	return ClassHandle.sensor_data;
+}
+static void init_ssp(void)
+{
+	SSP_CFG_Type SSP_ConfigStruct;
+	PINSEL_CFG_Type PinCfg;
+
+	/*
+	 * Initialize SPI pin connect
+	 * P0.7 - SCK;
+	 * P0.8 - MISO
+	 * P0.9 - MOSI
+	 * P2.2 - SSEL - used as GPIO
+	 */
+	PinCfg.Funcnum = 2;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 7;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 8;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 9;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Funcnum = 0;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
+
+	// Initialize SSP peripheral with parameter given in structure above
+	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
+
+	// Enable SSP peripheral
+	SSP_Cmd(LPC_SSP1, ENABLE);
+
+}
+static void init_i2c(void)
+{
+	PINSEL_CFG_Type PinCfg;
+
+	/* Initialize I2C2 pin connect */
+	PinCfg.Funcnum = 2;
+	PinCfg.Pinnum = 10;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 11;
+	PINSEL_ConfigPin(&PinCfg);
+
+	// Initialize I2C peripheral
+	I2C_Init(LPC_I2C2, 100000);
+
+	/* Enable I2C1 operation */
+	I2C_Cmd(LPC_I2C2, ENABLE);
+}
+
+void SysTick_Handler(void)
+{
+
+	ClassHandle.sensor_data =light_read();
+
+	Light_Interrupt_Done_Flag = 1;
+}
---
+++ bnc_oled/src/Sensor.h
@@ -0,0 +1,11 @@
+/*
+ * Sensor.h
+ */
+
+#ifndef SENSOR_H_
+#define SENSOR_H_
+
+void Sensor_new(void);
+int Sensor_read(void);
+
+#endif /* SENSOR_H_ */
